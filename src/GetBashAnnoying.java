5e7c5b9ed9afe65fc32581f07172d3341425a680
JT Reynolds <johnt.reynolds@outlook.com>
2025-08-30 23:50:46 -0600

    First Commit

---
+++ src/GetBashAnnoying.java
@@ -0,0 +1,13 @@
+public class GetBashAnnoying
+	{
+
+		public static void main(String[] args)
+			{
+				// TODO Auto-generated method stub
+				//i tired
+				System.out.println("Why");
+				System.out.println("This sucks");
+
+			}
+
+	}
